51b6653b6a79b6eb3b6a3655c48c5b3cf9af825c
AntonelaEspinola <antoespinola55@gmail.com>
2021-07-15 19:05:01 -0300

    Se uso de express generator para la organizacion de archivos y un motor de plantillas, EJS, para las vistas. Se implemento la modularizacion de codigo con ejs para las vistas.

---
+++ routes/index.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var router = express.Router();
+const mainController =require('../controllers/indexController');
+/* GET home page. */
+router.get('/', mainController.home);
+
+module.exports = router;
